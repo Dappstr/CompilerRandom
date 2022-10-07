ef7a2685f001623698f919eefd50cb575899ae67
Dappstr <87837549+Dappstr@users.noreply.github.com>
2022-10-06 23:11:49 -0700

    Update scanner.c

--- Comp1/scanner.c
+++ Comp1/scanner.c
@@ -1,6 +1,9 @@
 #include "stdlib.h"
 #include "data.h"
 #include "defs.h"
+#include "ctype.h"
+
+typedef struct Token Token;
 
 //Get next character
 static int nextChar(void)
@@ -21,5 +24,58 @@ static int nextChar(void)
         Line++;
         return c;
     }
+}
 
+static int skip(void)
+{
+    int c;
+
+    c = nextChar();
+    while(c == ' ' || c == '\t' || c == '\n' || c == '\n' || c == '\r' || c == '\f') {
+        c = nextChar();
+    }
+
+    return c;
+}
+
+int scan(Token* t)
+{
+    int c;
+    
+    //Skip whitespace
+    c = skip();
+
+    switch (c)
+    {
+        case EOF:
+            return 0;
+            break;
+    
+        case '+':
+            t->token = T_PLUS;
+            break;
+            
+        case '-':
+            t->token = T_MINUS;
+            break;
+
+        case '*':
+            t->token = T_MULT;
+            break;
+        
+        case '/':
+            t->token = T_DIVIDE;
+            break;
+
+        default:
+            if(isdigit(c))
+            {
+                t->iValue = scanint(c);
+                t->token = T_INTLIT;
+                break;
+            }
+        }
+
+    //Return 1 if token found.
+    return 1;
 }
